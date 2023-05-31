00b2faaf5978f6ec72e26a9d0547ffc02a6fa74c
ando <iamando@tuta.io>
2023-05-31 16:23:16 +0300

    feature: use cleye for managing cli interaction

--- src/cli.ts
+++ src/cli.ts
@@ -1,1 +1,12 @@
-console.log('Commit...')
+import { cli } from 'cleye'
+
+cli(
+  {
+    version: '1.0.0',
+    commands: [],
+  },
+  argv => {
+    const input = argv._.join(' ')
+    console.log(input)
+  }
+)
