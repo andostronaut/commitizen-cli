cb8bfc0a50d0481a849bc91644bdd904b73276ab
ando <iamando@tuta.io>
2023-06-01 20:37:58 +0300

    feature: setup commit command on exec child process

--- src/utils/commiter.ts
+++ src/utils/commiter.ts
@@ -1,8 +1,14 @@
+import { promisify } from 'node:util'
+import { exec } from 'node:child_process'
 import { cancel, intro, group } from '@clack/prompts'
 import { lightYellow } from 'kolorist'
 
 import { CANCELED_OP_MSG } from './constants'
 import { type, message } from './prompts'
+import { handleCliError, CliError } from './cli-errror'
+import { log } from './log'
+
+const execa = promisify(exec)
 
 export const commiter = async () => {
   intro(lightYellow('Commitizen CLI'))
@@ -20,6 +26,18 @@ export const commiter = async () => {
     }
   )
 
-  console.log(values.type)
-  console.log(values.message)
+  try {
+    const commit = `${values.type}: ${values.message}`
+    const cmd = `git commit -m ${commit}`
+    const { stdout, stderr } = await execa(cmd)
+
+    if (stderr) throw new CliError(`An error occured: ${stderr}`)
+
+    console.log(stdout)
+    console.log(commit)
+  } catch (err: any) {
+    log({ type: 'error', msg: err.message })
+    handleCliError(err)
+    process.exit(1)
+  }
 }
