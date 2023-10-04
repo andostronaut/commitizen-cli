6be4492e248bcb4ff8e49a173142e466944739f6
Ando <iamando@tuta.io>
2023-10-04 13:37:14 +0300

    feature: add hint for each type for more descriptive utilisation

--- src/utils/prompts.ts
+++ src/utils/prompts.ts
@@ -4,16 +4,63 @@ import * as p from '@clack/prompts'
 export const type = () =>
   p.select({
     message: 'Choose commit type',
+    initialValue: 'feat',
     options: [
-      { label: 'Feature', value: 'feature' },
-      { label: 'Bugfix', value: 'bugfix' },
-      { label: 'Hotfix', value: 'hotfix' },
-      { label: 'Chore', value: 'chore' },
-      { label: 'Epic', value: 'epic' },
-      { label: 'Design', value: 'design' },
-      { label: 'Experiment', value: 'experiment' },
-      { label: 'Documentation', value: 'documentation' },
-      { label: 'Refactor', value: 'refactor' },
+      {
+        label: 'Feat',
+        value: 'feat',
+        hint: 'For new features (e.g., feat: implement --cache-to feature to export cache)',
+      },
+      {
+        label: 'Fix',
+        value: 'fix',
+        hint: 'For improvements and bugfixes that do not introduce a feature (e.g., fix: improve error message)',
+      },
+      {
+        label: 'Hotfix',
+        value: 'hotfix',
+        hint: 'For improvements and bugfixes that do not introduce a feature, directly in prodution mode (e.g., fix: improve success message)',
+      },
+      {
+        label: 'Chore',
+        value: 'chore',
+        hint: 'General things that should be excluded (e.g., chore: clean up X)',
+      },
+      {
+        label: 'Design',
+        value: 'design',
+        hint: 'For design changes only (e.g., design: use rounded button)',
+      },
+      {
+        label: 'Experiment',
+        value: 'experiment',
+        hint: 'General things that should be in experiment (e.g., experiment: implement new query system in X)',
+      },
+      {
+        label: 'Docs',
+        value: 'docs',
+        hint: 'For documentation changes only (e.g., docs: fix typo in X)',
+      },
+      {
+        label: 'Refact',
+        value: 'refact',
+        hint: 'General things that should be restructured but not changing the orginal functionality (e.g., refact: move X to new file utils)',
+      },
+      {
+        label: 'CI',
+        value: 'ci',
+        hint: 'For internal CI specific changes (e.g., ci: enable X for tests)',
+      },
+      {
+        label: 'Infra',
+        value: 'infra',
+        hint: 'For infrastructure changes (e.g., infra: Enable cloudfront for X)',
+      },
+      {
+        label: 'Test',
+        value: 'test',
+        hint: 'For changes to tests only (e.g., test: check if X does Y)',
+      },
     ],
   }) as Promise<string>
 
